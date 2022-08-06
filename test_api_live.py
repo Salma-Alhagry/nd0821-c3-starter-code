2337b34066124957614959bcd38bab512597c506
Salma-Alhagry <salma.hagry@cu.edu.eg>
2022-08-06 13:33:08 +0200

    test_api_live.py

--- test_api_live.py
+++ test_api_live.py
@@ -1,27 +1,30 @@
 import requests
+import json
 
 url = "https://salma-udacity-mlops-p3.herokuapp.com/"
 
 response = requests.get(url)
 print(f"Status code: {response.status_code}")
 
-response = requests.post(url, json={
-    "summary": "Person",
-    "value":{
-        "age": 39,
-        "workclass": "State-gov",
-        "fnlgt": 77516,
-        "education": "Bachelors",
-        "education-num": 13,
-        "marital-status": "Never-married",
-        "occupation": "Adm-clerical",
-        "relationship": "Not-in-family",
-        "race": "White",
-        "sex": "Male",
-        "capital-gain": 2174,
-        "capital-loss": 0,
-        "hours-per-week": 40,
-        "native-country": "United-States",
-    }
-})
+data={
+    "age": 39,
+    "workclass": "State-gov",
+    "fnlgt": 77516,
+    "education": "Bachelors",
+    "education-num": 13,
+    "marital-status": "Never-married",
+    "occupation": "Adm-clerical",
+    "relationship": "Not-in-family",
+    "race": "White",
+    "sex": "Male",
+    "capital-gain": 2174,
+    "capital-loss": 0,
+    "hours-per-week": 40,
+    "native-country": "United-States",
+}
+data = json.dumps(data).encode('utf8')
+
+response = requests.post(url+"model", data)
+
 print(f"Status code: {response.status_code}")
+print(response.json())
